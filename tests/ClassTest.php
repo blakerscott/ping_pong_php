549ffd011d98e81d4d4827da846d991b683ccd41
Blake Scott and Jason Wuerch <blakerscott@gmail.com>
2016-02-15 10:35:32 -0800

    add second failing test

--- tests/ClassTest.php
+++ tests/ClassTest.php
@@ -17,6 +17,19 @@
 		//Assert
 		$this->assertEquals('1,2,3,4,5,6,7,8,9,10', $result);
 		}
+
+		function test_printPingPong()
+		{
+		//Arrange
+		$test_PingPongGenerator = new PingPongGenerator;
+		$input = '15';
+
+		//Act
+		$result = $test_PingPongGenerator->makeNumberList($input);
+
+		//Assert
+		$this->assertEquals('1,2,3,4,5,6,7,8,9,10,11,12,13,14,PINGPONG', $result);
+		}
 	}
 
 ?>
